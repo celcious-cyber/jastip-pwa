dd53e84b8ab116aea595b81aa9ce637a77aa2c3d
celcious-cyber <call.me.celcious@gmail.com>
2026-03-27 12:42:59 +0800

    fix: final pwa config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    react(),
+
+    // PWA Configuration
     VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['logo192.png'],
       manifest: {
         name: 'Jastip App',
         short_name: 'Jastip',
         description: 'Aplikasi Manajemen Jastip',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
         theme_color: '#ffffff',
         icons: [
           {
